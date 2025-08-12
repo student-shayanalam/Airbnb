2f5452328223b49981e0a4f00d92f7774f4728e1
student-shayanalam <smshayanalam@gmail.com>
2025-08-12 20:51:51 +0530

    feat: add show route for individual listings and create corresponding view

--- app.js
+++ app.js
@@ -25,14 +25,23 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//Index Route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
+});
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 // app.get("/testListing", async (req, res) => {
