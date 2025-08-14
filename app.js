fa0f4dede5d4d4566e20edb8c5793e05590ccb7e
student-shayanalam <smshayanalam@gmail.com>
2025-08-14 16:22:34 +0530

    feat: add EJS layout and components for navbar and footer, integrate CSS styles

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 
@@ -28,9 +29,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hi , I am root");
 });
 
 //Index Route
