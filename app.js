6db670fb3a6910d274bff48337915a0817582a93
student-shayanalam <smshayanalam@gmail.com>
2025-08-22 22:18:18 +0530

    feat: add dotenv configuration and session management with connect-mongo, update package dependencies

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-// app.js
-// This file sets up an Express server, connects to a MongoDB database,
-// and defines routes for the application.
-// It includes a sample route to save a listing to the database.
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const Listing = require("./models/listing.js");
+
+// require("dotenv").config(); // Load environment variables from .env file
+
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -22,7 +27,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -31,6 +36,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600, // time period in seconds
+});
+
+store.on("error", () => {
+  console.log("SESSION STORE ERROR" , err);
+});
+
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // 1 week
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+    httpOnly: true,
+  },
+};
+
 
 app.get("/", (req, res) => {
   res.send("Hi , I am root");
